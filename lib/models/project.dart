a47f13849dc0658408145ef49de9228bff4fd158
Ali Zare <alizaredev@gmail.com>
2026-06-10 00:00:19 +0330

    add project class

---
+++ lib/models/project.dart
@@ -0,0 +1,12 @@
+class Project {
+  final String title;
+  final String description;
+  final String image;
+  final String link;
+  Project({
+    required this.title,
+    required this.description,
+    required this.image,
+    required this.link,
+  });
+}
